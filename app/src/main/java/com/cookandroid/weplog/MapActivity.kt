58091f442db1ecfce37383ff07ac84704e3d0b8a
wwyyww <yso00302@gmail.com>
2021-09-07 23:54:49 +0900

    update main&map

---
+++ app/src/main/java/com/cookandroid/weplog/MapActivity.kt
@@ -0,0 +1,21 @@
+package com.cookandroid.weplog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast;
+import kotlinx.android.synthetic.main.activity_map.*
+
+
+
+class MapActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        map_start.setOnClickListener{
+            Toast.makeText(this@MapActivity, "Start 버튼 클릭", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+}
