9a657192f3f08dc8b9ccfe551aa10368a239d07a
HyeM207 <63229014+HyeM207@users.noreply.github.com>
2021-09-11 23:03:05 +0900

    ReConnect to Firebase DB

--- app/src/main/java/com/cookandroid/weplog/Login.kt
+++ app/src/main/java/com/cookandroid/weplog/Login.kt
@@ -1,13 +1,17 @@
 package com.cookandroid.weplog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.database.DatabaseReference
+import android.widget.Button
+
+
 
 class Login : AppCompatActivity() {
 
-    private lateinit var database: DatabaseReference
+    //private lateinit var database: DatabaseReference
 
+    lateinit var login_btn2 : Button
 
 
 
@@ -18,8 +22,17 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
+        //database = Firebase.database.reference
 
-        database = Firebase.database.reference
+        login_btn2 = findViewById(R.id.login_btn2)
+
+
+        login_btn2.setOnClickListener {
+            var intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+
 
     }
 }
---
+++ app/src/main/java/com/cookandroid/weplog/SignUp.kt
@@ -0,0 +1,15 @@
+package com.cookandroid.weplog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup)
+        setTitle("회원가입")
+
+
+
+    }
+}
