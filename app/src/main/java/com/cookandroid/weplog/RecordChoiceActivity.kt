e8933bac7fd976ff8a73b5468dcf8a27414ebcbf
jeongminhui99 <jeongminhee99@naver.com>
2021-09-26 00:48:43 +0900

    update mini

--- app/src/main/java/com/cookandroid/weplog/RecordChoiceActivity.kt
+++ app/src/main/java/com/cookandroid/weplog/RecordChoiceActivity.kt
@@ -7,5 +7,18 @@ class RecordChoiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.record_choice)
+        supportActionBar!!.hide()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        supportActionBar!!.show()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        supportActionBar!!.hide()
     }
 }
+
